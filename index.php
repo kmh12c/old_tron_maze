63d22b9f463e534eb242e476a3761986c9daccf9
kmh12c <kaylaholcomb@Kaylas-MacBook-Pro.local>
2016-05-03 18:49:07 -0500

    gl

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 			uniform sampler2D uSampler2;
 
 			void main(void) {
-				vec4 FragColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));
+				gl_FragColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));
 				vec4 FragColor2 = texture2D(uSampler2, vec2(vTextureCoord2.s, vTextureCoord2.t));
 			}
 		</script>
